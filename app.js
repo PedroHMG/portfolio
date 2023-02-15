b1b2b070fdee2f3aa596c909f28e1a4e88efd940
PedroHMG <goes.phmeirelles@gmail.com>
2023-02-14 22:58:05 -0300

    add header text with styling at the center and implemented sticky section

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
+
